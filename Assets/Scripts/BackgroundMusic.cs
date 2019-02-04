11759b40c5eed64550f35f0d38da8d02e4526e85
M-e-e <M-e-e@users.noreply.github.com>
2019-02-04 21:31:42 +0100

    major update
    +scenes merged
    +gameplay
    +music

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    static BackgroundMusic instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    float platformSpawnTime = 1f; //in secs
+    float timer=0;
+
+    public GameObject platform;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+        //creates a platform each platformSpawnTime-seconds
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            Instantiate(platform, new Vector3(Random.Range(-3f, 3), transform.position.y), Quaternion.identity);
+
+            timer = platformSpawnTime;
+        }
+    }
+}
