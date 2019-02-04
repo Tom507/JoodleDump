370f79ce395415a2688438b7620fe3ba1ca9c849
M-e-e <M-e-e@users.noreply.github.com>
2019-02-04 17:36:35 +0100

    platforms added
    +platforms
    +platformspawner
    
    //changed project to android

---
+++ Assets/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    float speed = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //removes the platform
+        Destroy(gameObject, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement
+        transform.position += Vector3.down * Time.deltaTime * speed;
+    }
+}
