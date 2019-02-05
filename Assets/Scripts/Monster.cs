bafde7e4ccfbb1f805b02b9bf7abf671bf6d9430
M-e-e <M-e-e@users.noreply.github.com>
2019-02-05 18:13:28 +0100

    monster edit
    +more spooky

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    GameManager gm;
+
+    void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "bullet")
+        {
+            Destroy(gameObject);
+        }
+        else if (other.name == "Joodler")
+        {
+            gm.endGame();
+        }
+    }
+}
